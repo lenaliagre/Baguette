fbb18dac260008b5d9c9ccae013048a631dfd15b
Roy Jou <36274083+TSPanda@users.noreply.github.com>
2018-05-11 18:55:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 const client = new Discord.Client();
 
-client.on('ready', => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity(config.defaultActivity);
 });
