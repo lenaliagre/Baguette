3c9aea616de826e37556c324107a37b4f82bf833
Roy Jou <36274083+TSPanda@users.noreply.github.com>
2018-05-11 17:53:44 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+
+require('events').EventEmitter.defaultMaxListeners = Infinity;
+
+const client = new Discord.Client();
+
+client.on('ready', => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(config.defaultActivity);
+});
+
+client.on('message', => {
+	const pref = msg.content.slice(0, 1);
+	const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+  
+  if (command === ping) {
+    msg.channel.send('Pong!');
+  }
+  
+  else {
+    return;
+  }
+});
+
+client.login(process.env.token);  
