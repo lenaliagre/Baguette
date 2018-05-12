96fb3d3da42904e556fd0dbce824cbf5fe36eb8d
lenaliagre <39158977+lenaliagre@users.noreply.github.com>
2018-05-12 18:27:25 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', msg => {
 		msg.channel.send("", {file:"https://i.pinimg.com/originals/aa/c9/d2/aac9d28cdac664c4916256010bd19127.gif"});	
 	}
 	else if (command === 'help') {
-		msg.channel.send(select('botcmdlist'));
+		msg.channel.send("", {file:"botcmdlist"});
 	}
 	else {
 		return;
