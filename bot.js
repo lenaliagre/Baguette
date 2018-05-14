a7fa0c7b9581695b767030b0a26aac492b15db26
TS_Panda <tspandaa@gmail.com>
2018-05-14 00:27:03 -0700

    dev

--- bot.js
+++ bot.js
@@ -5,11 +5,12 @@ const Music = require('discord.js-musicbot-addon');
 
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 
-const music = new Music(client, {youtubeKey: process.env.ytkey, //Youtube Dev API3 Key
-    prefix: config.prefix, maxQueueSize: 100, thumbnailType: 'default', deVolume: 100, anyoneCanSkip: true, messageHelp: true,
-    botOwner: '363749294362066945', helpCmd: 'assist', playCmd: 'play',    skipCmd: 'skip', queueCmd: 'queue', pauseCmd:  'pause', 
-        resumeCmd: 'resume', volumeCmd: 'vol', leaveCmd: 'leave', clearCmd: 'clear', setCmd: 'set', loopCmd: 'loop', searchCmd: 'search', 
-        ownerCmd: 'owner', enableQueueStat: true});
+const music = new Music(client, {
+	youtubeKey: process.env.ytapikey, //Youtube Dev API3 Key
+	prefix: config.prefix, maxQueueSize: 100, thumbnailType: 'default', defVolume: 100, anyoneCanSkip: true, messageHelp: true,
+	botOwner: '352384456977285130', helpCmd: 'assist', playCmd: 'play',	skipCmd: 'skip', queueCmd: 'queue',
+	pauseCmd: 'pause', resumeCmd: 'resume', volumeCmd: 'vol', leaveCmd: 'leave', clearCmd: 'clear', setCmd: 'set',
+	loopCmd: 'loop', searchCmd: 'search', ownerCmd: 'owner', enableQueueStat: true});
 
 const client = new Discord.Client();
 
