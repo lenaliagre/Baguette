eaf8df697356e4f25225d53d3ba2dcd1c206c461
lenaliagre <39158977+lenaliagre@users.noreply.github.com>
2018-05-21 00:34:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('message', msg => {
 				.setTitle('Here is some info *sends help* ^u^ -')
 				.addField('__Owner__ ~','Lena#1548')
 				.addField('__Prefix__ ~','~')
-				.addField('__Commands__ ~', 'Ping, Help, Stab, Eat, Punda, Drink, Duckymomo, Facedesk, Facepalm, Cri, Shrug, Tableflip, Slap, Llama, Hug, Gif, Coffee, Tea, Cuddle, Colesprouse, Cri, Punchmartin, 8ball')
+				.addField('__Commands__ ~', 'Ping, Help, Stab, Eat, Punda, Drink, Duckymomo, Facedesk, Facepalm, Cri, Shrug, Tableflip, Slap, Llama, Hug, Gif, Coffee, Tea, Cuddle, Colesprouse, Cri, 8ball')
 				.setColor(16775069)
 			msg.channel.sendEmbed(help);
 		}
