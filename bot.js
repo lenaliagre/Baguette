c49e2cae00579cf21d9de8cc436fc825bb2cd374
Lena Liagre <39158977+lenaliagre@users.noreply.github.com>
2018-06-20 15:45:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,7 @@ client.on('message', msg => {
 				.setTitle('Here is the command list for this bot! ^u^ -')
 				.addField('Actions ~', 'Stab, Neck, Eat, Drink, Hug, Facepalm, Facedesk, Cri, Tableflip, Slap, Cuddle, Smack')
 				.addField('Info ~', 'Help, Command')
+			        .addField('Music ~', 'Play, Skip, Pause, Resume, Leave, Loop, Queue')
 				.addField('Gifs ~', 'Colesprouse, Tomholland, Duckymomo, Llama, Gif')
 				.addField('Other ~', 'Yoke, Lenny, Ping, Punda, Shrug, Coffee, Tea, 8ball')
 				.setColor(0xffb295)
