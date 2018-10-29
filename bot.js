1fd30522f38490939ee1c88741a0c3d80d9a2464
Lena Liagre <39158977+lenaliagre@users.noreply.github.com>
2018-10-30 00:00:12 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on('message', msg => {
 		else if (command === 'commands') {
 			var help = new Discord.RichEmbed()
 			.setTitle('Here is the command list for this bot! ^u^ -')
-			.addField('Actions ~', 'Stab, Neck, Eat, Drink, Hug, Facepalm, Facedesk, Cri, Tableflip, Slap, Cuddle, Smack')
+			.addField('Actions ~', 'Greet, Stab, Neck, Eat, Drink, Hug, Facepalm, Facedesk, Cri, Tableflip, Slap, Cuddle, Smack')
 			.addField('Info ~', 'Help, Commands, Invite')
 			.addField('Music ~', 'Play, Skip, Pause, Resume, Leave, Loop, Queue')
 			.addField('Gifs ~', 'Colesprouse, Tomholland, Duckymomo, Llama, Gif')
